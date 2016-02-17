d24ecbbea5fbc67addcd301d73a28bcb7e59ceb1
Daniel Glover <gloversdaniel@gmail.com>
2016-02-17 12:04:46 -0700

    you mad bro

--- first_branch.rb
+++ first_branch.rb
@@ -3,6 +3,6 @@ puts "what is your favorite color?"
 favorite_color = gets.strip.downcase
 if favorite_color == 'black'
 	puts "you depressed bro?"
-elsif favorite_color == 'Red'
+elsif favorite_color== 'red'
 	puts "you mad bro?"
 end
