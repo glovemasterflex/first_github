f73d8f2de50954de9dfc86d946412465ddff0ce4
Daniel Glover <gloversdaniel@gmail.com>
2016-02-17 11:33:26 -0700

    this is my first branch. not ready for stable code.

---
+++ first_branch.rb
@@ -0,0 +1,11 @@
+puts "First Branch"
+puts "what is your favorite color?"
+favorite_color = gets.strip.downcase
+if favorite_color == 'black'
+	puts "you depressed bro?"
+elsif favorite_color == 'Red'
+	puts "you mad bro?"
+end
+# still need to grab user input
+# still need to output more info
+# still need to get more user input
